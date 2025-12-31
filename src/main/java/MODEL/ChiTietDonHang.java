edb6488f15e1b84daf8c2695d992ca25e822b548
NorSaH <167691649+NorSaH77@users.noreply.github.com>
2025-12-31 12:14:07 +0700

    Update ChiTietDonHang.java

--- src/main/java/MODEL/ChiTietDonHang.java
+++ src/main/java/MODEL/ChiTietDonHang.java
@@ -1,66 +1,33 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package MODEL;
 
-/**
- *
- * @author HP
- */
-
-
 public class ChiTietDonHang {
-    private String maDonHang;
-    private String maSanPham;
+    private int maDonHang;
+    private String maSP;
     private int soLuong;
     private double donGia;
-    private double thanhTien;
 
-    public ChiTietDonHang() { }
+    public ChiTietDonHang() {}
 
-    public ChiTietDonHang(String maDonHang, String maSanPham, int soLuong, double donGia) {
+    public ChiTietDonHang(int maDonHang, String maSP, int soLuong, double donGia) {
         this.maDonHang = maDonHang;
-        this.maSanPham = maSanPham;
+        this.maSP = maSP;
         this.soLuong = soLuong;
         this.donGia = donGia;
-        this.thanhTien = soLuong * donGia;
     }
 
-    public String getMaDonHang() { return maDonHang; }
-    public void setMaDonHang(String maDonHang) { this.maDonHang = maDonHang; }
+    public int getMaDonHang() { return maDonHang; }
+    public void setMaDonHang(int maDonHang) { this.maDonHang = maDonHang; }
 
-    public String getMaSanPham() { return maSanPham; }
-    public void setMaSanPham(String maSanPham) { this.maSanPham = maSanPham; }
+    public String getMaSP() { return maSP; }
+    public void setMaSP(String maSP) { this.maSP = maSP; }
 
     public int getSoLuong() { return soLuong; }
-    public void setSoLuong(int soLuong) {
-        this.soLuong = soLuong;
-        recalcThanhTien();
-    }
+    public void setSoLuong(int soLuong) { this.soLuong = soLuong; }
 
     public double getDonGia() { return donGia; }
-    public void setDonGia(double donGia) {
-        this.donGia = donGia;
-        recalcThanhTien();
-    }
+    public void setDonGia(double donGia) { this.donGia = donGia; }
 
-    public double getThanhTien() { return thanhTien; }
-    public void setThanhTien(double thanhTien) { this.thanhTien = thanhTien; }
-
-    private void recalcThanhTien() {
-        this.thanhTien = this.soLuong * this.donGia;
-    }
-
-    @Override
-    public String toString() {
-        return "ChiTietDonHang{" +
-                "maDonHang='" + maDonHang + '\'' +
-                ", maSanPham='" + maSanPham + '\'' +
-                ", soLuong=" + soLuong +
-                ", donGia=" + donGia +
-                ", thanhTien=" + thanhTien +
-                '}';
+    public double getThanhTien() {
+        return soLuong * donGia;
     }
 }
-
